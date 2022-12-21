9da2a1c108c710ff90d99b47dea8e66c0bf142e1
Tom Ordonez <tomordonezworks@gmail.com>
2022-12-20 19:23:04 -0600

    Created test case to validate search menu option

---
+++ readinglist/src/search.py
@@ -0,0 +1,12 @@
+class Search:
+    def __init__(self):
+        self.search_query = None
+
+    def get_search_query(self):
+        return self.search_query
+
+    def set_search_query(self, query):
+        self.search_query = query
+
+
+
